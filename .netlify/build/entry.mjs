1d27139cd6aeb68fab5f844f220cc939a627ba8a
Mauricio Sandoval <mauropsx@gmail.com>
2025-12-06 22:42:24 -0300

    Add Netlify adapter for deployment

---
+++ .netlify/build/entry.mjs
@@ -0,0 +1,34 @@
+import { renderers } from './renderers.mjs';
+import { s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CvSoi7hX.mjs';
+import { manifest } from './manifest_FK9yC7Tm.mjs';
+import { createExports } from '@astrojs/netlify/ssr-function.js';
+
+const serverIslandMap = new Map();;
+
+const _page0 = () => import('./pages/acerca.astro.mjs');
+const _page1 = () => import('./pages/servicios.astro.mjs');
+const _page2 = () => import('./pages/index.astro.mjs');
+const pageMap = new Map([
+    ["src/pages/acerca.astro", _page0],
+    ["src/pages/servicios.astro", _page1],
+    ["src/pages/index.astro", _page2]
+]);
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    actions: () => import('./_noop-actions.mjs'),
+    middleware: () => import('./_noop-middleware.mjs')
+});
+const _args = {
+    "middlewareSecret": "f1aa2861-b0ef-4ca2-87a9-04914089b2af"
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+const _start = 'start';
+if (Object.prototype.hasOwnProperty.call(serverEntrypointModule, _start)) {
+	serverEntrypointModule[_start](_manifest, _args);
+}
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
